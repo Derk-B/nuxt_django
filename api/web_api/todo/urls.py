337d51e73cd1cf987ad20b7bae2677ee02ac67ee
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2024-01-19 12:37:15 +0100

    Deleted old code

--- api/web_api/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("todos/", views.todos, name="todos"),
-    path("login/", views.login, name="login"),
-    path('user/', views.user, name="user"),
-    path("logout/", views.logout, name="logout")
-]
--- web/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <AppHeader />
-        <slot />
-    </div>
-</template>
